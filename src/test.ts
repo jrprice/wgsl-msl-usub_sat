a74c3399930a92420762468827373c6281ffec21
James Price <jrprice@google.com>
2023-11-13 15:24:01 -0500

    Add more tests and show results

--- src/test.ts
+++ src/test.ts
@@ -1,20 +1,43 @@
 interface TestConfig {
   name: string;
-  shader: string;
-  result: number;
+  code: string;
+  expected: number;
 }
 const kTests: TestConfig[] = [
   {
     name: "select",
-    shader: `
-@group(0) @binding(0) var<storage, read_write> buffer: u32;
-
-@compute @workgroup_size(1)
-fn main(@builtin(global_invocation_id) gid: vec3u) {
-  buffer = select(0u, gid.x - 1000u, gid.x > 1000u);
-}
-`,
-    result: 0,
+    code: "buffer = select(0u, gid.x - 1000u, gid.x > 1000u);",
+    expected: 0,
+  },
+  {
+    name: "add_sub_min_1",
+    code: "buffer = (zero + gid.x) - min(gid.x, 1000u);",
+    expected: 0,
+  },
+  {
+    name: "add_sub_min_2",
+    code: "buffer = (gid.x + zero) - min(1000u, zero);",
+    expected: 0,
+  },
+  {
+    name: "x_sub_min",
+    code: "buffer = gid.x - min(1000u, gid.x);",
+    expected: 0,
+  },
+  {
+    name: "c_sub_max",
+    code: "buffer = 1000u - max(gid.x, 1000u);",
+    expected: 0,
+  },
+  {
+    name: "min_sub_x",
+    code: "buffer = min(1000u, gid.x) - gid.x;",
+    expected: 0,
+  },
+  {
+    name: "max_sub_c",
+    code: "buffer = max(gid.x, 1000u) - 1000u;",
+    expected: 0,
   },
 ];
 
@@ -50,7 +73,17 @@ async function RunTest(cfg: TestConfig) {
   SetStatus(`Running '${cfg.name}'...`);
 
   // Compile the shader and create a compute pipeline.
-  const module = device.createShaderModule({ code: cfg.shader });
+  const code = `
+@group(0) @binding(0) var<storage, read_write> buffer: u32;
+@group(0) @binding(1) var<uniform> zero: u32;
+
+@compute @workgroup_size(1)
+fn main(@builtin(global_invocation_id) gid: vec3u) {
+  _ = zero;
+  ${cfg.code}
+}
+`;
+  const module = device.createShaderModule({ code });
   const pipeline = await device.createComputePipeline({
     compute: { module, entryPoint: "main" },
     layout: "auto",
@@ -67,8 +100,19 @@ async function RunTest(cfg: TestConfig) {
     usage: GPUBufferUsage.MAP_READ | GPUBufferUsage.COPY_DST,
   });
   let data = new Uint32Array(buffer.getMappedRange());
-  data[0] = 0xffffffff;
+  data[0] = 0xdeadbeef;
   buffer.unmap();
+
+  const zero = device.createBuffer({
+    size: 4,
+    usage: GPUBufferUsage.UNIFORM,
+    mappedAtCreation: true,
+  });
+  {
+    let data = new Uint32Array(zero.getMappedRange());
+    data[0] = 0;
+    zero.unmap();
+  }
 
   // Dispatch the shader to the device.
   const commands = device.createCommandEncoder();
@@ -77,7 +121,10 @@ async function RunTest(cfg: TestConfig) {
   pass.setBindGroup(
     0,
     device.createBindGroup({
-      entries: [{ binding: 0, resource: { buffer } }],
+      entries: [
+        { binding: 0, resource: { buffer } },
+        { binding: 1, resource: { buffer: zero } },
+      ],
       layout: pipeline.getBindGroupLayout(0),
     })
   );
@@ -89,9 +136,19 @@ async function RunTest(cfg: TestConfig) {
   // Read back the result.
   await staging.mapAsync(GPUMapMode.READ);
   let result = new Uint32Array(staging.getMappedRange());
-  // TODO: Display result on page
-  console.log(`result = ${result[0]}`);
+  let got = result[0];
   staging.unmap();
+
+  const passed = cfg.expected === got;
+
+  const table = document.getElementById("table");
+  let row = "<tr>";
+  row += `<td>${cfg.name}</td>`;
+  row += `<td>${cfg.expected}</td>`;
+  row += `<td>${got}</td>`;
+  row += `<td style="color: ${passed ? "green" : "red"}">${passed ? "Pass" : "Fail"}</td>`;
+  row += "</tr>";
+  table.innerHTML += row;
 }
 
 Run();
