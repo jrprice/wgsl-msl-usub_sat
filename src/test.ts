8d5f5d5067598ac1839be14a3d8d2535a28013a4
James Price <jrprice@google.com>
2023-11-13 18:11:14 -0500

    Show code in table

--- src/test.ts
+++ src/test.ts
@@ -155,6 +155,7 @@ fn main(@builtin(global_invocation_id) gid: vec3u) {
   row += `<td>${cfg.expected}</td>`;
   row += `<td>${got}</td>`;
   row += `<td style="color: ${passed ? "green" : "red"}">${passed ? "Pass" : "Fail"}</td>`;
+  row += `<td style="font-family: monospace">${cfg.code}</td>`;
   row += "</tr>";
   table.innerHTML += row;
 
