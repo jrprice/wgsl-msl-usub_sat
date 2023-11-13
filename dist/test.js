d3cbac08bceff864dcccec9df5cef6924346c8c3
jrprice <jamesprice.dev@gmail.com>
2023-11-13 21:11:28 +0000

    Deploying to gh-pages from  @ 2bdbcb199c3fcd6989c9cd5458485e40ea283891 🚀

--- dist/test.js
+++ dist/test.js
@@ -37,6 +37,7 @@ const kTests = [
 ];
 let adapter;
 let device;
+let markdown_results;
 const status_label = document.getElementById("status");
 function SetStatus(status) {
     status_label.textContent = status;
@@ -53,6 +54,8 @@ async function Run() {
         SetStatus("Failed to create WebGPU device.");
         return;
     }
+    markdown_results = "| Name | Expected | Got | Pass/Fail |\n";
+    markdown_results += "| --- | --- | --- | --- |";
     kTests.forEach((cfg) => {
         RunTest(cfg);
     });
@@ -128,5 +131,10 @@ fn main(@builtin(global_invocation_id) gid: vec3u) {
     row += `<td style="color: ${passed ? "green" : "red"}">${passed ? "Pass" : "Fail"}</td>`;
     row += "</tr>";
     table.innerHTML += row;
+    markdown_results += `\n| ${cfg.name} | ${cfg.expected} | ${got} | ${passed ? "Pass" : "Fail"} |`;
 }
+document.getElementById("copy-results").addEventListener("click", () => {
+    navigator.clipboard.writeText(markdown_results);
+    document.getElementById("copied").style.display = "";
+});
 Run();
